8e48566c2a4c4f0cf414b618b8cec9277d9258df
배정현 <bae1004kin@gmail.com>
2023-09-05 05:44:41 +0900

    add rabbit MQ and config server

--- ApiGateway/src/main/java/com/example/scg/ScgApplication.java
+++ ApiGateway/src/main/java/com/example/scg/ScgApplication.java
@@ -1,7 +1,10 @@
 package com.example.scg;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ScgApplication {
@@ -10,4 +13,9 @@ public class ScgApplication {
         SpringApplication.run(ScgApplication.class, args);
     }
 
+
+    @Bean
+    public HttpTraceRepository httpTraceRepository(){
+        return new InMemoryHttpTraceRepository();
+    }
 }
